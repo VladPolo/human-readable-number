398cb8e01fee7886d8c8f13be73ec7020ecf4eb5
VladPolo <polovets_vlad@mail.ru>
2023-01-29 20:25:52 +0300

    ffgd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function toReadable (number) {
     const
     h = ['hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'],
-    t = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+    t = ['', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
     o = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
     p = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seveteen', 'eighteen', 'nineteen'];
 let str = number.toString(), out = '';
@@ -12,7 +12,7 @@ else if(str.length == 2){
     else out = (t[parseInt(str[0])-1] + ((str[1]!='0')?(' ' + o[parseInt(str[1])-1]):''));
 }
 else if(str.length == 3){
-    out = (h[parseInt(str[0])-1] + ((str[1]!='0')?(' ' + t[parseInt(str[1])-1]):'') + ((str[2]!='0')?(' ' + o[parseInt(str[2])-1]):''));
+    out = (h[parseInt(str[0])-1] + ((str[1]!='0')?(' ' + t[parseInt(str[1])-1]):'') + ((str[2]!='0')?(' ' + o[parseInt(str[2])-1]):'teen'));
 }
 
 let arr = out.split('');
